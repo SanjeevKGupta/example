6c70e0f5fc7ee6e2493362b4fe41b72896edcdf4
Sanjeev Gupta <sanjeev@ibm.com>
2019-04-29 21:28:50 -0700

    Basic MQTT Kafka for IBM Event Stream

---
+++ efg/service/mqtt_kafka/src/mqtt_kafka.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+mosquitto_sub -h mqtt_broker -t iot-2/type/+/id/+/evt/random/fmt/json | while read -r line
+do
+  if [ ! -z "$line" ]; then 
+    d=$(echo "$line" | jq -r '.d')
+
+    id=$(echo "$d" | jq -r '.id' )
+    intensity=$(echo "$d"  | jq -r '.i' )
+
+    epoch=$(date +%s)
+    json='{"date":'$epoch',"id":"'$id'","i":'$intensity'}'
+
+    echo "$json" 
+    
+    echo "$json" | kafkacat -P -b $EVENT_STREAM_BROKER_URL -X api.version.request=true -X security.protocol=sasl_ssl -X sasl.mechanisms=PLAIN -X sasl.username=${EVENT_STREAM_API_KEY:0:16} -X sasl.password=${EVENT_STREAM_API_KEY:16} -t "$EVENT_STREAM_EFG_TOPIC"
+#     kafkacat "${json}" -P -b $EVENT_STREAM_BROKER_URL -X api.version.request=true -X security.protocol=sasl_ssl -X sasl.mechanisms=PLAIN -X sasl.username=${EVENT_STREAM_API_KEY:0:16} -X sasl.password=${EVENT_STREAM_API_KEY:16} -t "$EVENT_STREAM_EFG_TOPIC"
+  fi
+done
+

bc0a1cbf89b4182776077c90aaa7092249ebcb1e
Sanjeev Gupta <sanjeev@ibm.com>
2019-05-08 23:02:54 -0700

    BLE support in --net=host mode

--- svc/mqtt_kafka/src/mqtt_kafka.sh
+++ svc/mqtt_kafka/src/mqtt_kafka.sh
@@ -9,12 +9,11 @@ do
     intensity=$(echo "$d"  | jq -r '.i' )
 
     epoch=$(date +%s)
-    json='{"date":'$epoch',"id":"'$id'","i":'$intensity'}'
+    json='{"date":'$epoch',"id":"'$id'","light":'$intensity'}'
 
     echo "$json" 
     
     echo "$json" | kafkacat -P -b $EVENT_STREAM_BROKER_URL -X api.version.request=true -X security.protocol=sasl_ssl -X sasl.mechanisms=PLAIN -X sasl.username=${EVENT_STREAM_API_KEY:0:16} -X sasl.password=${EVENT_STREAM_API_KEY:16} -t "$EVENT_STREAM_EFG_TOPIC"
-#     kafkacat "${json}" -P -b $EVENT_STREAM_BROKER_URL -X api.version.request=true -X security.protocol=sasl_ssl -X sasl.mechanisms=PLAIN -X sasl.username=${EVENT_STREAM_API_KEY:0:16} -X sasl.password=${EVENT_STREAM_API_KEY:16} -t "$EVENT_STREAM_EFG_TOPIC"
   fi
 done
 
